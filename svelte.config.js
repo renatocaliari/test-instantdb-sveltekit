db1d7b40b968c96a8d56194673f663a3aca9207b
Cali <renatoac82@gmail.com>
2024-06-07 19:03:08 -0300

    ,

--- svelte.config.js
+++ svelte.config.js
@@ -14,8 +14,8 @@ const config = {
 		adapter: adapter({
 			// default options are shown. On some platforms
 			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
+			pages: 'public',
+			assets: 'public',
 			fallback: 'index.html',
 			precompress: false,
 			strict: true
